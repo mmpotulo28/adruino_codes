bce8cfd6d2ad053148a7d142dc0939ea1ab48acb
Manelisi Mpotulo <mpotulom28@gmail.com>
2024-02-07 12:22:52 +0200

    Update adruino_botton_pressed#1.c

--- adruino_botton_pressed#1.c
+++ adruino_botton_pressed#1.c
@@ -1,38 +1,52 @@
 bool btn;
 int btnPressed = -1;
 bool alreadyPressed = false;
-  
+int count = 0;
+int greenToggle = -1;
+
 void setup()
 {
-  pinMode(13, OUTPUT);
-  pinMode(6, INPUT);
-  Serial.begin(9600);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(6, INPUT);
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
 }
 
-void loop()
-{
+void loop(){
+  // toggle Yellow LED
+  toggleYellowLED();
+
   btn = digitalRead(6);
   
-  if(btn){
-    if(!alreadyPressed){
-    	btnPressed = btnPressed * -1;
-    	alreadyPressed = true;
-   	}
+  if (btn){
+    if (!alreadyPressed){
+      btnPressed = btnPressed * -1;
+      alreadyPressed = true;
+    }
   }else{
     alreadyPressed = false;
   }
-    
-    
   
+  //toggle LEDs
   
-  
-  if(btnPressed == 1){
-    Serial.println("ON");
+  if (btnPressed == 1){
     digitalWrite(13, HIGH);
-  } else if(btnPressed == -1){
-    Serial.println("OFF");
+  }else if (btnPressed == -1){
     digitalWrite(13, LOW);
   }
   
   delay(200);
+}
+
+void toggleYellowLED(){
+  count++;
+  if (count % 2 == 0){
+    greenToggle *= -1;
+    if (greenToggle == 1){
+      digitalWrite(2, HIGH);
+    }else{
+      digitalWrite(2, LOW);
+    }
+  }
 }
